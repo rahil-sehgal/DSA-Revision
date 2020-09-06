cf9db67bc8c2bc22984acb054695f45361db1f4d
RAHIL SEHGAL <57448644+rahil-sehgal@users.noreply.github.com>
2020-09-07 00:32:53 +0530

    Update 22. Generate Parentheses.cpp

--- Backtracking/22. Generate Parentheses.cpp
+++ Backtracking/22. Generate Parentheses.cpp
@@ -8,9 +8,20 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         helper(res,"",0,0,n); //start with a blank string ""
-        return helper;
+        return res;
     }
-    void helper(vector<string> res,){
+    void helper(vector<string> res,string s,int open,int close,int n){
+        if(s.length()==n*2){
+           // res.insert (res.begin(), s);
+           res.push_back("s") ;
+            return;
+        }
+        if(open<n){
+            helper(res,s+"(",open+1,close,n);
+        }
+        if(close<open){
+            helper (res,s+")",open,close+1,n);
+        }
         
-    }
+    }  
 };
