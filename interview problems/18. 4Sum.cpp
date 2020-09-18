1426d5f701a4afea8706324639ecbe676688cd47
RAHIL SEHGAL <57448644+rahil-sehgal@users.noreply.github.com>
2020-09-18 12:01:56 +0530

    incomplete

---
+++ interview problems/18. 4Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& a, int target) {
+        
+        
+        for(int j=0;j<n;j++){
+            sum=target -a[j];
+            
+            
+            
+        }
+    }
+};
