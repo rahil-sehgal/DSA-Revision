b6fb2f4f3dc7912c0910ed7d739c167367ae0ba7
RAHIL SEHGAL <57448644+rahil-sehgal@users.noreply.github.com>
2020-10-01 15:53:19 +0530

    Create B. Find Marble.cpp

---
+++ a2oj/B/B. Find Marble.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include <stdio.h> 
+#include<math.h>
+using namespace std;
+ 
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+
+ int main() 
+{ 
+  
+  FIO
+    
+  int n,s,t;
+  cin>>n>>s>>t;
+  int p[n+1];
+  int ans =0;
+  //s --> initail pos
+  //t --> final pos
+
+  for (int i = 1; i <= n; ++i)
+  {
+    cin>>p[i];
+  }
+  
+  while(s!=t && p[s]!=0){
+    //it is 1 indexing 
+    int temp=p[s];
+    p[s]=0;
+    s=temp;
+      ans++;
+
+  }
+  if(s==t){
+  cout<<ans<<endl;
+}
+else cout<<-1<<endl;
+
+  return 0;
+
+}
